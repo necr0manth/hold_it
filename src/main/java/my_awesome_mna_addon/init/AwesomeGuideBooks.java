96e0cd8543583a446c32793e73971bf4990e1310
Tamerik0 <100145032+Tamerik0@users.noreply.github.com>
2025-06-05 17:33:25 +0300

    Fixed guidebook namespace

--- src/main/java/my_awesome_mna_addon/init/AwesomeGuideBooks.java
+++ src/main/java/my_awesome_mna_addon/init/AwesomeGuideBooks.java
@@ -1,6 +1,7 @@
 package my_awesome_mna_addon.init;
 
 import com.mna.api.guidebook.RegisterGuidebooksEvent;
+import my_awesome_mna_addon.MyAwesomeMnaAddon;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -9,6 +10,6 @@ import net.minecraftforge.fml.common.Mod;
 public interface AwesomeGuideBooks {
 	@SubscribeEvent
 	static void onRegisterGuidebooks(RegisterGuidebooksEvent event) {
-		event.getRegistry().addGuidebookPath(new ResourceLocation("examplemod", "guide"));
+		event.getRegistry().addGuidebookPath(new ResourceLocation(MyAwesomeMnaAddon.MODID, "guide"));
 	}
 }
