485f5304ac95cf55111a6473d910838d5d2ae25a
Necr0manth <100145032+Tamerik0@users.noreply.github.com>
2025-06-16 09:15:32 +0300

    Added event handler

---
+++ src/main/java/dev/dsai03/hold_it/events/ShitEventHandler.java
@@ -0,0 +1,17 @@
+package dev.dsai03.hold_it.events;
+
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public class ShitEventHandler {
+
+	@SubscribeEvent
+	public static void shit(TickEvent.ServerTickEvent event) {
+//		int a = 0;
+//		for(int i = 0;i<1000000000;i++){
+//			a+=i;
+//		}
+	}
+}
