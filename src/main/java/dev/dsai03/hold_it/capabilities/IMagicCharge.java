fa87a0358ae113729509c81a5bd12b444bfa1155
Necr0manth <100145032+Tamerik0@users.noreply.github.com>
2025-06-16 14:29:49 +0300

    Removed unused magic charge capability

--- src/main/java/dev/dsai03/hold_it/capabilities/IMagicCharge.java
+++
@@ -1,9 +0,0 @@
-package dev.dsai03.hold_it.capabilities;
-
-import net.minecraftforge.common.capabilities.AutoRegisterCapability;
-
-@AutoRegisterCapability
-public interface IMagicCharge {
-	int getCharge();
-	void setCharge(int amount);
-}
